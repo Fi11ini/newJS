efd37024dfc0190f2d792f310c5f24f4ec1576e1
Andrey <andreymdm@gmail.com>
2023-12-17 22:30:53 +0300

    11.objecd

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
+//Массив оценок студента
 
-const name = 'Andrey';
-const secondName = 'Krish';
-
-const proFile = {
-    name,
-    secondName,
-    age: 31,
-}
-
-console.log(proFile)
+let grade 1 = 5;
+let grade 2 = 4;
+let grade3 = 3;
+let grade4 = 5;
+let grade5 = 4;
+console.log(grade1); //5 - это оценка за первый предмет
+console.log(grade3); //3 - это оценка за третий предмет
