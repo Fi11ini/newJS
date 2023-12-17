98e67d2675f140a59cdac013021edf19e6f569ad
Andrey <andreymdm@gmail.com>
2023-12-18 00:30:11 +0300

    11

--- 03.js
+++ 03.js
@@ -1,9 +1,14 @@
 //Массив оценок студента
-
-let grade 1 = 5;
-let grade 2 = 4;
+let grade1 = 5;
+let grade2 = 4;
 let grade3 = 3;
 let grade4 = 5;
 let grade5 = 4;
 console.log(grade1); //5 - это оценка за первый предмет
 console.log(grade3); //3 - это оценка за третий предмет
+
+
+let grades = [ 5, 4, 3, 5, 4 ];
+
+
+console.log(grades)
