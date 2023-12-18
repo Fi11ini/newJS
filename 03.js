99bb8e3b3a61b2835287a765277af2706b257b73
Andrey <andreymdm@gmail.com>
2023-12-19 00:45:38 +0300

    13

--- 03.js
+++ 03.js
@@ -7,8 +7,8 @@ let grade5 = 4;
 console.log(grade1); //5 - это оценка за первый предмет
 console.log(grade3); //3 - это оценка за третий предмет
 
+let grades = [5, 4, 3, 5, 4];
 
-let grades = [ 5, 4, 3, 5, 4 ];
+console.log(grades);
 
 
-console.log(grades)
