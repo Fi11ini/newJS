97cb9abf64c95d5ffc20f92e015cc8b321621c1d
Andrey <andreymdm@gmail.com>
2023-12-19 03:21:41 +0300

    12

---
+++ testMassive.js
@@ -0,0 +1,19 @@
+let memberOne = { name: "Andrey", surname: "Krish", age: 31 };
+let memberTwo = { name: "Vadim", surname: "Krish", age: 30 };
+let memberThree = { name: "Polina", surname: "Krish", age: 29 };
+let memberFour = { name: "Tonya", surname: "Krish", age: 62 };
+let memberFive = { name: "Nikita", surname: "Krish", age: 13 };
+let memberSix = { name: "Marik", surname: "Krish", age: 2 };
+
+let myFamily = [
+{ name: "Andrey", surname: "Krish", age: 31 }
+]
+
+console.log(myFamily[0].age)
+
+
+
+
+
+
+
