3587d803c4d2aa326c0f8f0faa57a86cc82d84ae
Andrey <andreymdm@gmail.com>
2024-01-16 19:53:02 +0300

    fidddddddddddddd

--- start/js.js
+++ start/js.js
@@ -1,57 +1,17 @@
-'use strict';
+'use strickt';
 
-// let li = document.querySelectorAll("li");
+let massive = [1, 2, 5, 4];
+let massiveSecond = [2, 3, 5, 5, 5];
+let desert = [];
 
-// let styleOfHeader = getComputedStyle(li, "::before");
-// let header = document.querySelector("h1");
-
-// console.log();
-
-// function removeChar(str) {
-//     const newStr = str.substr(1, str.length - 2);
-//     return newStr;
-// }
-
-// removeChar();
-
-let numbers = [1, 2, 2];
-
-// function squareSum(numbers) {
-//     let total = 0;
-//     for (let i = 0; i < numbers.length; i++) {
-//         numbers[i] += total;
-//         return total;
-//     }
-// }
-
-// squareSum();
-// console.log(total);
-
-// function basicOp(operator, value1, value2) {
-//     switch (operator) {
-//         case '+':
-//             return value1 + value2;
-//         case '-':
-//             return value1 - value2;
-//         case '*':
-//             return value1 * value2;
-//         case '/':
-//             return value1 / value2;
-//         default:
-//             return 0;
-//     }
-// }
-
-// console.log(basicOp('-', 2, 3));
-
-function lovefunc(flower1, flower2) {
-    let result;
-    if ((flower1 + flower2) % 2 === 1) {
-        result = true;
-    } else if ((flower1 + flower2) % 2 === 0) {
-        result = false;
-    }
-    return result;
+function findAverage(numbers) {
+    const initialValue = 0;
+    const sumWithInitial = numbers.reduce(
+        (accumulator, currentValue) => accumulator + currentValue,
+        initialValue
+    );
+    return initialValue;
 }
 
-console.log(lovefunc(1, 2));
+console.log(findAverage(massive));
+// Expected output: 10
