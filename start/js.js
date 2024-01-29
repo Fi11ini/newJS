53e1833ed1dfd4519c8cb23531570ab864a1d5ec
Andrey <andreymdm@gmail.com>
2024-01-30 02:15:09 +0300

    doubleLetter

--- start/js.js
+++ start/js.js
@@ -1,21 +1,12 @@
-// const balance = 120;
-// const bonusBalance = 900;
-// const isBanned = true;
-// const isExist = false;
-// const isSelling = false;
+let string = 'Vadim'
 
-// const canBuy = (balance > 1000 || bonusBalance > 100) && !isBanned && !isExist;
-// console.log(`Могу купить игру: ${canBuy ? 'Да' : 'Нет'}`);
-
-let array = [1, 2, 3, 5];
-
-function simpleMultiplication(number) {
-    let newNumber = 0;
-    if (number % 2 === 0) {
-        newNumber = number * 8;
-    } else {
-        newNumber = number * 9;
-    }
-    return newNumber;
+function doubleChar(str) {
+    let newString = str.split('')
+    .map((letter) => letter + letter )
+    .join('')
+    return newString
 }
-console.log(simpleMultiplication(1));
+
+
+
+console.log(doubleChar(string));
