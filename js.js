48b4d79f9b4c1c7d521b7f86fd0e53fbaa4c0d97
Andrey <andreymdm@gmail.com>
2024-02-10 02:17:09 +0300

    codewars cm/h

--- js.js
+++ js.js
@@ -1,6 +1,5 @@
-function checkForFactor (base, factor) {
-    return base % factor === 0;
+function cockroachSpeed(s) {
+    return Math.round(s * 100000/3600)
 }
 
-console.log(checkForFactor(9, 2))
-
+console.log(cockroachSpeed(1.09))
