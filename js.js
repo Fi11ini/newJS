ffd2d49de292ed5bf1d8c46654b8f096c2d70f66
Andrey <andreymdm@gmail.com>
2024-02-11 23:52:20 +0300

    codewars task

--- js.js
+++ js.js
@@ -1,11 +1,13 @@
-function between(a, b) {
-   let arr = [];
-
-   for (let i = a; i <= b; i++) {
-      arr.push(i);
+function finalGrade (exam, projects) {
+   if(exam > 90 || projects > 10) {
+      return 100
+   }else if (exam > 75 && projects >= 5) {
+      return 90
+   }else if(exam > 50 && projects >= 2) {
+      return 75
+   } else {
+      return 0
    }
-   return arr
 }
 
-
-console.log(between(-1, 7))
+console.log(finalGrade(85, 5))
