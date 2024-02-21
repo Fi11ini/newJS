00f9eee4b2ac929d267222c9d9471c19839806f7
Andrey <andreymdm@gmail.com>
2024-02-21 03:24:14 +0300

    codewars task4

--- js.js
+++ js.js
@@ -1,11 +1,22 @@
-function finalGrade (exam, projects) {
-   if(exam > 90 || projects > 10) {
-      return 100
-   }else if (exam > 75 && projects >= 5) {
-      return 90
-   }else if(exam > 50 && projects >= 2) {
-      return 75
-   } else {
-      return 0
-   }
+let test = 'ooxx'
+
+
+function checkXOBalance(inputStr) {
+    inputStr = inputStr.toLowerCase();
+
+    let countX = 0;
+    let countO = 0;
+
+    for (let i = 0; i < inputStr.length; i++) {
+        if (inputStr[i] === 'x') {
+            countX++;
+        } else if (inputStr[i] === 'o') {
+            countO++;
+        }
+    }
+    return countX === countO;
 }
+
+
+const result = checkXOBalance(test);
+console.log(result);
